cfb306096bbc0204ed3f5973a75f9c5baa60d83e
luisfelipevalencia <70112728+luisfelipevalencia@users.noreply.github.com>
2022-07-11 16:56:20 -0500

    modulo profesores
    creacion del modulo de profesores

--- sitioWeb/apps/plataforma/models.py
+++ sitioWeb/apps/plataforma/models.py
@@ -1,3 +1,102 @@
 from django.db import models
 
 # Create your models here.
+
+class Escuela(models.Model):
+    id_escuela = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    telefono_escuela = models.CharField(max_length=15)
+
+class Profesor(models.Model):
+    id_profesor = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=50)
+    primer_apellido = models.CharField(max_length=50)
+    segundo_apellido = models.CharField(max_length=50)
+    sexos = [
+        ('F','Femenino'),
+        ('M', 'Masculino')
+    ]
+    sexo_profesor = models.CharField(max_length=1, choices=sexos, default='F')
+    email_profesor = models.EmailField()
+    telefono_profesor = models.CharField(max_length=15)
+    cargos = [('Profesor','Profesor'), ('Rector','Rector'), ('Administrador','Administrador')]
+    cargo = models.CharField(max_length=20, choices=cargos, default='Profesor')
+    estudios = [('Tecnico','Tecnico'),('Licenciatura','Licenciatura'),('Pregrado','Pregrado'),('Maestria','Maestria'),('Doctorado','Doctorado')]
+    nivel_estudios = models.CharField(max_length=20, choices=estudios, default='Licenciatura')
+    password = models.CharField(max_length=20)
+    vigencia = models.BooleanField(default=True)
+
+class Acudiente(models.Model):
+    id_acudiente = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=50)
+    primer_apellido = models.CharField(max_length=50)
+    segundo_apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
+    vigencia = models.BooleanField(default=True)
+
+
+class Curso(models.Model):
+    id_curso = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=50)
+    periodo_electivo = models.CharField(max_length=6)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+
+class Asignatura(models.Model):
+    id_asignatura = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=50)
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+
+
+class Estudiante(models.Model):
+    id_estudiante = models.BigIntegerField (primary_key=True)
+    nombre = models.CharField(max_length=50)
+    primer_apellido = models.CharField(max_length=50)
+    segundo_apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    sexos = [
+        ('F','Femenino'),
+        ('M', 'Masculino')
+    ]
+    sexo = models.CharField(max_length=1, choices=sexos, default='F')
+    id_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    id_acudiente = models.ForeignKey(Acudiente, on_delete=models.CASCADE)
+    vigencia = models.BooleanField(default=True)
+
+
+class Nota(models.Model):
+    id_nota = models.BigIntegerField (primary_key=True)
+    calificacion = models.DecimalField(max_digits=3, decimal_places=2)
+    periodo = models.CharField(max_length=6)
+    fecha = models.DateField()
+    tipos = [('Parcial','Parcial'),('Quiz','Quiz'),('Tarea','Tarea'),('Trabajo','Trabajo')]
+    tipo = models.CharField(max_length=20, choices=tipos, default='Tarea')
+    comentario  = models.TextField()
+    id_asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+    id_estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+
+class Cita(models.Model):
+    id_cita = models.BigIntegerField (primary_key=True)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    id_acudiente = models.ForeignKey(Acudiente, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+
+class Sugerencia(models.Model):
+    id_sugerencia = models.BigIntegerField (primary_key=True)
+    fecha_sugerencia = models.DateField()
+    detalle_sugerencia = models.TextField()
+    id_acudiente = models.ForeignKey(Acudiente, on_delete=models.CASCADE)
+
+
+class Evento(models.Model):
+    id_evento = models.BigIntegerField (primary_key=True)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    detalle = models.TextField()
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
--- sitioWeb/apps/plataforma/views.py
+++ sitioWeb/apps/plataforma/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profesor
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html',{})
+
+def profesores(request):
+    profesores = Profesor.objects.all()
+    return render(request,"gestionProfesor.html",{"profesores":profesores})
+
+
+
+def registrarProfesor(request):
+
+    ident = request.POST['numeroId']
+    nombre = request.POST['txtNombre']
+    apellido1 = request.POST['txtPrimerApellido']
+    apellido2 = request.POST['txtSegundoApellido']
+    sexo = request.POST['Sexo']
+    email = request.POST['txtEmail']
+    telefono = request.POST['txtTelefono']
+    cargo = request.POST['Cargo']
+    estudio = request.POST['Estudio']
+    password = request.POST['pwd']
+    activo = request.POST['Activo']
+
+    profesor = Profesor.objects.create(id_profesor=ident, nombre=nombre, primer_apellido=apellido1, segundo_apellido=apellido2,
+    sexo_profesor = sexo, email_profesor=email ,telefono_profesor=telefono, cargo=cargo, nivel_estudios=estudio, password=password, vigencia=activo)
+
+    return redirect('/profesores')
+
+def edicionProfesor(request, ident):
+    profesor = Profesor.objects.get(id_profesor=ident)
+    return render(request,"actualizarProfesor.html", {"profesor":profesor})
+
+def editarProfesor(request):
+    ident = request.POST['numeroId']
+    nombre = request.POST['txtNombre']
+    apellido1 = request.POST['txtPrimerApellido']
+    apellido2 = request.POST['txtSegundoApellido']
+    sexo = request.POST['Sexo']
+    email = request.POST['txtEmail']
+    telefono = request.POST['txtTelefono']
+    cargo = request.POST['Cargo']
+    estudio = request.POST['Estudio']
+    password = request.POST['pwd']
+    activo = request.POST['Activo']
+
+    profesor = Profesor.objects.get(id_profesor=ident)
+
+    profesor.id_profesor = ident
+    profesor.nombre = nombre
+    profesor.primer_apellido = apellido1
+    profesor.segundo_apellido = apellido2
+    profesor.sexo_profesor = sexo
+    profesor.email_profesor = email
+    profesor.telefono_profesor = telefono
+    profesor.cargo = cargo
+    profesor.nivel_estudios = estudio
+    profesor.password = password
+    profesor.vigencia = activo
+    profesor.save()
+    return redirect('/profesores')
+
+
+def eliminarProfesor(request, ident):
+    profesor = Profesor.objects.get(id_profesor = ident)
+    profesor.delete()
+    return redirect('/profesores')
