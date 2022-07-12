26f6a030ba3b88f68d1c4b27fbd51f2fad6f0192
Tim <53590485+DiegoTimana@users.noreply.github.com>
2022-07-11 19:00:25 -0500

    actualizo correo del cliente

--- sitioWeb/sitioWeb/settings.py
+++ sitioWeb/sitioWeb/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dierom021@gmail.com"
-EMAIL_HOST_PASSWORD = "zzezpnrlhmablvrz"
+EMAIL_HOST_USER = "paginapuerchag@gmail.com"
+EMAIL_HOST_PASSWORD = "ozklgkbtajoukpcl"
