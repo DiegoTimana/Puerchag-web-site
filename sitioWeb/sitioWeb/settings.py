a00430c39114c4bbc20338e374e114db04d9f29b
Tim <alexis.timana@correounivalle.edu.co>
2022-08-04 18:24:55 -0500

    intento despliegue

--- sitioWeb/sitioWeb/settings.py
+++ sitioWeb/sitioWeb/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlewarel.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'sitioWeb.urls'
